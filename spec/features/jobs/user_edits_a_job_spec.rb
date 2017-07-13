e95ba59e8387b46a077f258e3912bc196d091931
Michael Centrelli <Acentrelli@gmail.com>
2017-07-13 11:56:57 -0600

    Passing user edit test

--- spec/features/jobs/user_edits_a_job_spec.rb
+++ spec/features/jobs/user_edits_a_job_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 RSpec.feature "User edits an existing job" do
   scenario "they see the updated data for the individual job" do
-    Company.create(name: "Johnson & Johnson")
-    job = Job.create(title: "Basket Weaver", description: "Weaving baskets with your toes", level_of_interest: 4, city: "San Jose", company_id: 1)
+    company = Company.create(name: "Johnson & Johnson")
+    job = Job.create(title: "Basket Weaver", level_of_interest: 4, company_id: company.id, city: "San Jose")
     updated_title = "Master BW"
 
-    visit '/companies/1/jobs/1'
-    click_on("Edit")
-    fill_in "job_name", with: updated_title
-    click_on "Update job"
+    visit companies_path
+    click_on "Johnson & Johnson"
+    click_on "Basket Weaver"
+    click_on "Edit"
+    fill_in "job_title", with: updated_title
+    click_on "Update Job"
 
     expect(page).to have_content(updated_title)
     expect(page).to_not have_content(job.title)
