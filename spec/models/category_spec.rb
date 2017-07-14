581d594ee0d4358929f95fb7a6153227dae5af2d
Michael Centrelli <Acentrelli@gmail.com>
2017-07-13 18:19:55 -0600

    Test Pass for Category Model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Category do
+  describe "attributes" do
+    it "has a title" do
+      category = build(:category)
+      expect(category).to respond_to(:title)
+    end
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_uniqueness_of(:title) }
+  end
+
+  describe "relationships" do
+    it { should have_many(:jobs) }
+  end
+
 end
